ce1b2b4d935076afd489c2a0284b17300adf1468
Erik <3kdapp25.norder@folkuniversitetet.nu>
2025-10-15 14:27:42 +0200

    Added print functionality to Main class

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Calculator calculator = new Calculator();
+
+        System.out.println("2 + 2 = " + calculator.add(2, 2));
+        System.out.println("2 - 2 = " + calculator.subtract(2, 2));
+        System.out.println("2 * 2 = " + calculator.multiply(2, 2));
+        System.out.println("2 / 2 = " + calculator.divide(2, 2));
+
     }
 }
